387ab1edf8a726feee8c6b30548747efa0bf8a57
Alif RenG <alifsuria98@gmail.com>
2023-08-06 16:12:26 +0800

    upd

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/alifsuria.github.io/dist/",
+  base:"/dist/",
   plugins: [
     vue(),
   ],
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: '/alifsuria.github.io/dist/',
+    publicPath: '/dist/',
   };
